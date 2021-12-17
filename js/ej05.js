9a005f9d3ee01504637a5e0e9b6f3d599f1d230f
VictoriaBesnati <83200428+VictoriaBesnati@users.noreply.github.com>
2021-12-17 15:30:17 -0300

    Add files via upload

---
+++ js/ej05.js
@@ -0,0 +1,20 @@
+// collapsible
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+// barra de navegacion
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+//Carusel
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+// ampliar imagen
+$(document).ready(function(){
+   $('.materialboxed').materialbox();
+ });
+//tooltips
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
